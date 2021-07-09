1b8e679b28418c2316c711d2257dccbf51d5f8b4
Yuri Gonçalves <65895389+yuriv15@users.noreply.github.com>
2021-07-08 22:00:10 -0300

    v1.2 - Score implementation

--- js/interface.js
+++ js/interface.js
@@ -13,7 +13,19 @@ function handleClick(event) {
     let square = event.target;
     let position = square.id;
 
+    let scoreP1 = document.getElementById("score-p1");
+    let scoreP2 = document.getElementById("score-p2");
+    let scoreP1Number = parseInt(scoreP1.innerText);
+    let scoreP2Number = parseInt(scoreP2.innerText);
+
     if(handleMove(position)){
+        if(playerTime == 0){
+            scoreP1Number += 1;
+            scoreP1.innerText = scoreP1Number;
+        }else if(playerTime == 1){
+            scoreP2Number += 1;
+            scoreP2.innerText = scoreP2Number;
+        }
         setTimeout(() => {
             alert ("O jogador " + (playerTime+1) + " venceu!");
         }, 10);
